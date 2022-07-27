f499e4c17c09b7abedc8ac1e57404cc984e06538
KimH4nKyul <alskim93@kakao.com>
2022-07-27 15:43:34 +0900

    mod code

--- TIL/go-workspace/src/my/jedi/level1/quiz4.go
+++ TIL/go-workspace/src/my/jedi/level1/quiz4.go
@@ -7,13 +7,13 @@ type MyInt int
 var x MyInt
 
 func QuizFour() {
-    fmt.Println(x)
-    fmt.Printf("%T\n",x)
-    x = 42
-//     x = int(42)
-    fmt.Println(x)
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	//     x = int(42)
+	fmt.Println(x)
 }
 
 func main() {
-    QuizFour()
+	QuizFour()
 }
